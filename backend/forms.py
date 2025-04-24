623b8f5ad76dd8e3f496878e2582ba8375b389bc
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-04-24 11:49:03 +0530

    Create forms.py

---
+++ backend/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post,Comment
+from taggit.forms import TagField
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title','slug','content','category','tags']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = ['Comment']
