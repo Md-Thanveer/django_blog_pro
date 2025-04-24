587246272e2b29f159c4f85dda8e5ba5c7508b2d
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-04-24 12:36:20 +0530

    Add Slug feature.

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,19 @@
+from webbrowser import register
+
 from django.contrib import admin
 
+from backend.models import Category, Post, Comment
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+    # search_fields = ('name',)
+#
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'created_at']
+
+    prepopulated_fields = {'slug': ('title',)}
+admin.site.register(Comment)
